34c3dd96ae6cb8aee0b95af141744509b136ee44
김민주 <77184157+kim-min-ju449@users.noreply.github.com>
2022-08-01 13:25:13 +0900

    8.1 commit

---
+++ src/d1/Name.js
@@ -0,0 +1,38 @@
+// import React, {useState} from "react";
+
+// function Name(props){
+//     const [name, setName] = useState('');
+//     const [gender, setGender] = useState('');
+
+//     const = (event) =>{
+//         setName
+//     }
+//     const = (event) =>{
+//         setGender(event.target.value);
+
+//     };
+//     const  = (event) =>{
+//         alert('이름: ${name} 성별:${gender}');
+//         event.preventDefault();
+//     };
+//     return(
+//         <form onSubmit={handleSubmit}>
+//             <br/>
+            
+//             <label style={{padding:16}}>
+//                 이름:
+//                 <input type="text" value={text} onChange={handleChangeName}/>
+
+//             </label>
+//             <br></br>
+//             <label style={{padding:16}}>
+//                 성별:
+//                 <select value={} onChange={handleChangeGender}>
+//                     <option value="남자">남자</option>
+//                     <option value="여자">여자</option>
+//                 </select>
+//             </label>
+//         </form>
+//     )
+// }
+// export default Name;
